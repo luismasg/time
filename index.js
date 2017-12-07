8e7613bb93bf0b17831e788110a2438bf8323c85
Luis Suarez <lsuarez@cbtnuggets.com>
2017-12-06 22:36:24 -0600

    slightly wierd but better

--- index.js
+++ index.js
@@ -7,47 +7,17 @@ setInterval(() => {
 }, 1000);
 
 function tick() {
-  var seconds = document.querySelectorAll('.time')[3];
-  var secondsValue = parseInt(seconds.innerHTML, 10);
-  if (secondsValue <= 58) {
-    seconds.innerHTML = secondsValue + 1;
-    return;
-  }
-
-  seconds.innerHTML = 0;
-  increaseMinutes();
-}
-function increaseMinutes() {
-  var minutes = document.querySelectorAll('.time')[2];
-  var minutesValue = parseInt(minutes.innerHTML, 10);
-  if (minutesValue <= 58) {
-    minutes.innerHTML = minutesValue + 1;
-    return;
-  }
-
-  minutes.innerHTML = 0;
-  increaseHours();
+  if (increase(3, 59))
+    if (increase(2, 59)) if (increase(1, 24)) increase(1, 24);
 }
 
-function increaseHours() {
-  var hours = document.querySelectorAll('.time')[1];
-  var hoursValue = parseInt(hours.innerHTML, 10);
-  if (hoursValue <= 23) {
-    hours.innerHTML = hoursValue + 1;
-    return;
+function increase(unit, max) {
+  var time = document.querySelectorAll('.time')[unit];
+  var timeValue = parseInt(time.innerHTML, 10);
+  if (timeValue <= max - 1) {
+    time.innerHTML = timeValue + 1;
+    return false;
   }
-
-  hours.innerHTML = 0;
-  increaseDays();
-}
-
-function increaseDays() {
-  var days = document.querySelectorAll('.time')[0];
-  var daysValue = parseInt(days.innerHTML, 10);
-  if (daysValue <= 23) {
-    days.innerHTML = daysValue + 1;
-    return;
-  }
-
-  days.innerHTML = 0;
+  time.innerHTML = 0;
+  return true;
 }
